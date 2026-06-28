c21d9e4a94da52ece7485ff0938fa471e97b3b13
Bytuna <ilker.tuna@yandex.com>
2026-06-28 14:10:15 +0300

    Speed Insights eklendi ve son düzenlemeler yapıldı

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata: Metadata = {
   title: "Mikat-ı Nur",
